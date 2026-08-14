d9378c51a80e11521536611e7e3109877603fdf6
Pankaj Patel <pankajlucky678@gmail.com>
2026-08-14 21:49:58 +0530

    feat(Likhitha): add root node.py entrypoint

---
+++ node.py
@@ -0,0 +1,9 @@
+"""
+MeshWeaver Node Entry Point (Execution Track - Person B / Likhitha)
+"""
+
+import asyncio
+from meshweaver.node import cli_main
+
+if __name__ == "__main__":
+    asyncio.run(cli_main())
